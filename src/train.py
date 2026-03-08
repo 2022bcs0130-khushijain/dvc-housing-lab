e84735f350ef96e42f61b77a44324f16f8c3a62c
Khushi-KJ <duplicaradhika29feb04@gmail.com>
2026-03-08 19:53:47 +0530

    Version 2 - Full dataset

--- src/train.py
+++ src/train.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 # Load dataset
-df = pd.read_csv("data/housing.csv")
+df = pd.read_csv("data/full_housing.csv")
 
 # Fill missing values
 df = df.fillna(df.mean(numeric_only=True))
